ebe687e389a35240dd82ec151bdf01e4d32fbfcc
JiangKelong <624693615@qq.com>
2020-03-07 21:09:13 +0800

    创建页脚组件

---
+++ components/ViewFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">
+    <b-container>
+      Copyright © Jiang All Rights Reserved.
+      <a href="http://www.beian.miit.gov.cn" target="_blank"
+        >皖ICP备16025511号-1</a
+      >
+    </b-container>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,18 +4,21 @@
     <b-container class="main-container">
       <b-row>
         <b-col class="b-col" lg="9"> <nuxt /></b-col>
-        <b-col class="b-col" lg="3"><side /></b-col>
+        <b-col class="b-col" lg="3"><view-side /></b-col>
       </b-row>
     </b-container>
+    <view-footer />
   </div>
 </template>
 <script>
 import Navbar from '~/components/Navbar.vue'
-import Side from '~/components/Side.vue'
+import ViewSide from '~/components/ViewSide.vue'
+import ViewFooter from '~/components/ViewFooter.vue'
 export default {
   components: {
     Navbar,
-    Side
+    ViewSide,
+    ViewFooter
   }
 }
 </script>
